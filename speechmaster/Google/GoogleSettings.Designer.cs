dc5558c61ae8b640484bcb9ac716cc08883d17bf
Fax <aaron@aaron-m.co.nz>
2024-07-30 01:18:17 +1200

    google settings

--- speechmaster/Google/GoogleSettings.Designer.cs
+++ speechmaster/Google/GoogleSettings.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace speechmaster
+﻿
+namespace speechmaster
 {
     partial class GoogleSettings
     {
@@ -28,10 +29,445 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            panel1 = new Panel();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            flowLayoutPanel3 = new FlowLayoutPanel();
+            label3 = new Label();
+            typeInput = new TextBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            label1 = new Label();
+            projectIdInput = new TextBox();
+            flowLayoutPanel4 = new FlowLayoutPanel();
+            label2 = new Label();
+            privateKeyIdInput = new TextBox();
+            flowLayoutPanel5 = new FlowLayoutPanel();
+            label4 = new Label();
+            privateKeyInput = new TextBox();
+            flowLayoutPanel6 = new FlowLayoutPanel();
+            label5 = new Label();
+            clientEmailInput = new TextBox();
+            flowLayoutPanel7 = new FlowLayoutPanel();
+            label6 = new Label();
+            clientIdInput = new TextBox();
+            flowLayoutPanel8 = new FlowLayoutPanel();
+            label7 = new Label();
+            authUriInput = new TextBox();
+            flowLayoutPanel9 = new FlowLayoutPanel();
+            label8 = new Label();
+            tokenUriInput = new TextBox();
+            flowLayoutPanel10 = new FlowLayoutPanel();
+            label9 = new Label();
+            authProviderX509CertUrlInput = new TextBox();
+            flowLayoutPanel11 = new FlowLayoutPanel();
+            label10 = new Label();
+            clientX509CertUrlInput = new TextBox();
+            saveButton = new Button();
+            panel1.SuspendLayout();
+            flowLayoutPanel2.SuspendLayout();
+            flowLayoutPanel3.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            flowLayoutPanel4.SuspendLayout();
+            flowLayoutPanel5.SuspendLayout();
+            flowLayoutPanel6.SuspendLayout();
+            flowLayoutPanel7.SuspendLayout();
+            flowLayoutPanel8.SuspendLayout();
+            flowLayoutPanel9.SuspendLayout();
+            flowLayoutPanel10.SuspendLayout();
+            flowLayoutPanel11.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(flowLayoutPanel2);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(938, 735);
+            panel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.Controls.Add(flowLayoutPanel3);
+            flowLayoutPanel2.Controls.Add(flowLayoutPanel1);
+            flowLayoutPanel2.Controls.Add(flowLayoutPanel4);
+            flowLayoutPanel2.Controls.Add(flowLayoutPanel5);
+            flowLayoutPanel2.Controls.Add(flowLayoutPanel6);
+            flowLayoutPanel2.Controls.Add(flowLayoutPanel7);
+            flowLayoutPanel2.Controls.Add(flowLayoutPanel8);
+            flowLayoutPanel2.Controls.Add(flowLayoutPanel9);
+            flowLayoutPanel2.Controls.Add(flowLayoutPanel10);
+            flowLayoutPanel2.Controls.Add(flowLayoutPanel11);
+            flowLayoutPanel2.Controls.Add(saveButton);
+            flowLayoutPanel2.Dock = DockStyle.Fill;
+            flowLayoutPanel2.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel2.Location = new Point(0, 0);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(938, 735);
+            flowLayoutPanel2.TabIndex = 2;
+            // 
+            // flowLayoutPanel3
+            // 
+            flowLayoutPanel3.AutoSize = true;
+            flowLayoutPanel3.Controls.Add(label3);
+            flowLayoutPanel3.Controls.Add(typeInput);
+            flowLayoutPanel3.Dock = DockStyle.Fill;
+            flowLayoutPanel3.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel3.Location = new Point(3, 3);
+            flowLayoutPanel3.Name = "flowLayoutPanel3";
+            flowLayoutPanel3.Size = new Size(426, 44);
+            flowLayoutPanel3.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(30, 15);
+            label3.TabIndex = 2;
+            label3.Text = "type";
+            // 
+            // typeInput
+            // 
+            typeInput.Location = new Point(3, 18);
+            typeInput.Name = "typeInput";
+            typeInput.Size = new Size(420, 23);
+            typeInput.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoSize = true;
+            flowLayoutPanel1.Controls.Add(label1);
+            flowLayoutPanel1.Controls.Add(projectIdInput);
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel1.Location = new Point(3, 53);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(426, 44);
+            flowLayoutPanel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 15);
+            label1.TabIndex = 2;
+            label1.Text = "project_id";
+            // 
+            // projectIdInput
+            // 
+            flowLayoutPanel1.SetFlowBreak(projectIdInput, true);
+            projectIdInput.Location = new Point(3, 18);
+            projectIdInput.Name = "projectIdInput";
+            projectIdInput.Size = new Size(420, 23);
+            projectIdInput.TabIndex = 0;
+            // 
+            // flowLayoutPanel4
+            // 
+            flowLayoutPanel4.AutoSize = true;
+            flowLayoutPanel4.Controls.Add(label2);
+            flowLayoutPanel4.Controls.Add(privateKeyIdInput);
+            flowLayoutPanel4.Dock = DockStyle.Fill;
+            flowLayoutPanel4.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel4.Location = new Point(3, 103);
+            flowLayoutPanel4.Name = "flowLayoutPanel4";
+            flowLayoutPanel4.Size = new Size(426, 44);
+            flowLayoutPanel4.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(81, 15);
+            label2.TabIndex = 2;
+            label2.Text = "private_key_id";
+            // 
+            // privateKeyIdInput
+            // 
+            privateKeyIdInput.Location = new Point(3, 18);
+            privateKeyIdInput.Name = "privateKeyIdInput";
+            privateKeyIdInput.Size = new Size(420, 23);
+            privateKeyIdInput.TabIndex = 0;
+            // 
+            // flowLayoutPanel5
+            // 
+            flowLayoutPanel5.AutoSize = true;
+            flowLayoutPanel5.Controls.Add(label4);
+            flowLayoutPanel5.Controls.Add(privateKeyInput);
+            flowLayoutPanel5.Dock = DockStyle.Fill;
+            flowLayoutPanel5.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel5.Location = new Point(3, 153);
+            flowLayoutPanel5.Name = "flowLayoutPanel5";
+            flowLayoutPanel5.Size = new Size(426, 44);
+            flowLayoutPanel5.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(3, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 15);
+            label4.TabIndex = 2;
+            label4.Text = "private_key";
+            // 
+            // privateKeyInput
+            // 
+            privateKeyInput.Location = new Point(3, 18);
+            privateKeyInput.Name = "privateKeyInput";
+            privateKeyInput.Size = new Size(420, 23);
+            privateKeyInput.TabIndex = 0;
+            // 
+            // flowLayoutPanel6
+            // 
+            flowLayoutPanel6.AutoSize = true;
+            flowLayoutPanel6.Controls.Add(label5);
+            flowLayoutPanel6.Controls.Add(clientEmailInput);
+            flowLayoutPanel6.Dock = DockStyle.Fill;
+            flowLayoutPanel6.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel6.Location = new Point(3, 203);
+            flowLayoutPanel6.Name = "flowLayoutPanel6";
+            flowLayoutPanel6.Size = new Size(426, 44);
+            flowLayoutPanel6.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(3, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(70, 15);
+            label5.TabIndex = 2;
+            label5.Text = "client_email";
+            // 
+            // clientEmailInput
+            // 
+            clientEmailInput.Location = new Point(3, 18);
+            clientEmailInput.Name = "clientEmailInput";
+            clientEmailInput.Size = new Size(420, 23);
+            clientEmailInput.TabIndex = 0;
+            // 
+            // flowLayoutPanel7
+            // 
+            flowLayoutPanel7.AutoSize = true;
+            flowLayoutPanel7.Controls.Add(label6);
+            flowLayoutPanel7.Controls.Add(clientIdInput);
+            flowLayoutPanel7.Dock = DockStyle.Fill;
+            flowLayoutPanel7.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel7.Location = new Point(3, 253);
+            flowLayoutPanel7.Name = "flowLayoutPanel7";
+            flowLayoutPanel7.Size = new Size(426, 44);
+            flowLayoutPanel7.TabIndex = 7;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(3, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(51, 15);
+            label6.TabIndex = 2;
+            label6.Text = "client_id";
+            // 
+            // clientIdInput
+            // 
+            clientIdInput.Location = new Point(3, 18);
+            clientIdInput.Name = "clientIdInput";
+            clientIdInput.Size = new Size(420, 23);
+            clientIdInput.TabIndex = 0;
+            // 
+            // flowLayoutPanel8
+            // 
+            flowLayoutPanel8.AutoSize = true;
+            flowLayoutPanel8.Controls.Add(label7);
+            flowLayoutPanel8.Controls.Add(authUriInput);
+            flowLayoutPanel8.Dock = DockStyle.Fill;
+            flowLayoutPanel8.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel8.Location = new Point(3, 303);
+            flowLayoutPanel8.Name = "flowLayoutPanel8";
+            flowLayoutPanel8.Size = new Size(426, 44);
+            flowLayoutPanel8.TabIndex = 8;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(3, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(50, 15);
+            label7.TabIndex = 2;
+            label7.Text = "auth_uri";
+            // 
+            // authUriInput
+            // 
+            authUriInput.Location = new Point(3, 18);
+            authUriInput.Name = "authUriInput";
+            authUriInput.Size = new Size(420, 23);
+            authUriInput.TabIndex = 0;
+            authUriInput.Text = "https://accounts.google.com/o/oauth2/auth";
+            // 
+            // flowLayoutPanel9
+            // 
+            flowLayoutPanel9.AutoSize = true;
+            flowLayoutPanel9.Controls.Add(label8);
+            flowLayoutPanel9.Controls.Add(tokenUriInput);
+            flowLayoutPanel9.Dock = DockStyle.Fill;
+            flowLayoutPanel9.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel9.Location = new Point(3, 353);
+            flowLayoutPanel9.Name = "flowLayoutPanel9";
+            flowLayoutPanel9.Size = new Size(426, 44);
+            flowLayoutPanel9.TabIndex = 9;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(3, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(56, 15);
+            label8.TabIndex = 2;
+            label8.Text = "token_uri";
+            // 
+            // tokenUriInput
+            // 
+            tokenUriInput.Location = new Point(3, 18);
+            tokenUriInput.Name = "tokenUriInput";
+            tokenUriInput.Size = new Size(420, 23);
+            tokenUriInput.TabIndex = 0;
+            tokenUriInput.Text = "https://oauth2.googleapis.com/token";
+            // 
+            // flowLayoutPanel10
+            // 
+            flowLayoutPanel10.AutoSize = true;
+            flowLayoutPanel10.Controls.Add(label9);
+            flowLayoutPanel10.Controls.Add(authProviderX509CertUrlInput);
+            flowLayoutPanel10.Dock = DockStyle.Fill;
+            flowLayoutPanel10.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel10.Location = new Point(3, 403);
+            flowLayoutPanel10.Name = "flowLayoutPanel10";
+            flowLayoutPanel10.Size = new Size(426, 44);
+            flowLayoutPanel10.TabIndex = 10;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(3, 0);
+            label9.Name = "label9";
+            label9.Size = new Size(153, 15);
+            label9.TabIndex = 2;
+            label9.Text = "auth_provider_x509_cert_url";
+            // 
+            // authProviderX509CertUrlInput
+            // 
+            authProviderX509CertUrlInput.Location = new Point(3, 18);
+            authProviderX509CertUrlInput.Name = "authProviderX509CertUrlInput";
+            authProviderX509CertUrlInput.Size = new Size(420, 23);
+            authProviderX509CertUrlInput.TabIndex = 0;
+            authProviderX509CertUrlInput.Text = "https://www.googleapis.com/oauth2/v1/certs";
+            // 
+            // flowLayoutPanel11
+            // 
+            flowLayoutPanel11.AutoSize = true;
+            flowLayoutPanel11.Controls.Add(label10);
+            flowLayoutPanel11.Controls.Add(clientX509CertUrlInput);
+            flowLayoutPanel11.Dock = DockStyle.Fill;
+            flowLayoutPanel11.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel11.Location = new Point(3, 453);
+            flowLayoutPanel11.Name = "flowLayoutPanel11";
+            flowLayoutPanel11.Size = new Size(426, 44);
+            flowLayoutPanel11.TabIndex = 11;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(3, 0);
+            label10.Name = "label10";
+            label10.Size = new Size(109, 15);
+            label10.TabIndex = 2;
+            label10.Text = "client_x509_cert_url";
+            // 
+            // clientX509CertUrlInput
+            // 
+            clientX509CertUrlInput.Location = new Point(3, 18);
+            clientX509CertUrlInput.Name = "clientX509CertUrlInput";
+            clientX509CertUrlInput.Size = new Size(420, 23);
+            clientX509CertUrlInput.TabIndex = 0;
+            clientX509CertUrlInput.Text = "https://www.googleapis.com/robot/v1/metadata/x509/server%40talk-bork.iam.gserviceaccount.com";
+            // 
+            // saveButton
+            // 
+            saveButton.Location = new Point(3, 503);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new Size(75, 23);
+            saveButton.TabIndex = 12;
+            saveButton.Text = "Save";
+            saveButton.UseVisualStyleBackColor = true;
+            saveButton.Click += saveButton_Click_1;
+            // 
+            // GoogleSettings
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel1);
+            Name = "GoogleSettings";
+            Size = new Size(938, 735);
+            panel1.ResumeLayout(false);
+            flowLayoutPanel2.ResumeLayout(false);
+            flowLayoutPanel2.PerformLayout();
+            flowLayoutPanel3.ResumeLayout(false);
+            flowLayoutPanel3.PerformLayout();
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            flowLayoutPanel4.ResumeLayout(false);
+            flowLayoutPanel4.PerformLayout();
+            flowLayoutPanel5.ResumeLayout(false);
+            flowLayoutPanel5.PerformLayout();
+            flowLayoutPanel6.ResumeLayout(false);
+            flowLayoutPanel6.PerformLayout();
+            flowLayoutPanel7.ResumeLayout(false);
+            flowLayoutPanel7.PerformLayout();
+            flowLayoutPanel8.ResumeLayout(false);
+            flowLayoutPanel8.PerformLayout();
+            flowLayoutPanel9.ResumeLayout(false);
+            flowLayoutPanel9.PerformLayout();
+            flowLayoutPanel10.ResumeLayout(false);
+            flowLayoutPanel10.PerformLayout();
+            flowLayoutPanel11.ResumeLayout(false);
+            flowLayoutPanel11.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private TextBox type;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private FlowLayoutPanel flowLayoutPanel3;
+        private Label label3;
+        private TextBox typeInput;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label label1;
+        private TextBox projectIdInput;
+        private FlowLayoutPanel flowLayoutPanel4;
+        private Label label2;
+        private TextBox privateKeyIdInput;
+        private FlowLayoutPanel flowLayoutPanel5;
+        private Label label4;
+        private TextBox privateKeyInput;
+        private FlowLayoutPanel flowLayoutPanel6;
+        private Label label5;
+        private TextBox clientEmailInput;
+        private FlowLayoutPanel flowLayoutPanel7;
+        private Label label6;
+        private TextBox clientIdInput;
+        private FlowLayoutPanel flowLayoutPanel8;
+        private Label label7;
+        private TextBox authUriInput;
+        private FlowLayoutPanel flowLayoutPanel9;
+        private Label label8;
+        private TextBox tokenUriInput;
+        private FlowLayoutPanel flowLayoutPanel10;
+        private Label label9;
+        private TextBox authProviderX509CertUrlInput;
+        private FlowLayoutPanel flowLayoutPanel11;
+        private Label label10;
+        private TextBox clientX509CertUrlInput;
+        private Button saveButton;
     }
 }
--- speechmaster/Google/GoogleSettings.cs
+++ speechmaster/Google/GoogleSettings.cs
@@ -15,6 +15,34 @@ namespace speechmaster
         public GoogleSettings()
         {
             InitializeComponent();
+
+            typeInput.Text = GoogleAuthSettings.Instance.Type;
+            projectIdInput.Text = GoogleAuthSettings.Instance.ProjectId;
+            privateKeyIdInput.Text = GoogleAuthSettings.Instance.PrivateKeyId;
+            privateKeyInput.Text = GoogleAuthSettings.Instance.PrivateKey;
+            clientEmailInput.Text = GoogleAuthSettings.Instance.ClientEmail;
+            clientIdInput.Text = GoogleAuthSettings.Instance.ClientId;
+            authUriInput.Text = GoogleAuthSettings.Instance.AuthUri;
+            tokenUriInput.Text = GoogleAuthSettings.Instance.TokenUri;
+            authProviderX509CertUrlInput.Text = GoogleAuthSettings.Instance.AuthProviderX509CertUrl;
+            clientX509CertUrlInput.Text = GoogleAuthSettings.Instance.ClientX509CertUrl;
+        }
+
+
+        private void saveButton_Click_1(object sender, EventArgs e)
+        {
+            GoogleAuthSettings.Instance.Type = typeInput.Text;
+            GoogleAuthSettings.Instance.ProjectId = projectIdInput.Text;
+            GoogleAuthSettings.Instance.PrivateKeyId = privateKeyIdInput.Text;
+            GoogleAuthSettings.Instance.PrivateKey = privateKeyInput.Text;
+            GoogleAuthSettings.Instance.ClientEmail = clientEmailInput.Text;
+            GoogleAuthSettings.Instance.ClientId = clientIdInput.Text;
+            GoogleAuthSettings.Instance.AuthUri = authUriInput.Text;
+            GoogleAuthSettings.Instance.TokenUri = tokenUriInput.Text;
+            GoogleAuthSettings.Instance.AuthProviderX509CertUrl = authProviderX509CertUrlInput.Text;
+            GoogleAuthSettings.Instance.ClientX509CertUrl = clientX509CertUrlInput.Text;
+
+            GoogleAuthSettings.Instance.Save();
         }
     }
 }
